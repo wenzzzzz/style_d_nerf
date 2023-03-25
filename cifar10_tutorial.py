9e9a244255cb12789dac0865c93c440850d66797
Wenzhao Li <wenzhaoli2022camcs@gmail.com>
2023-03-25 21:34:43 +0000

    fix: dataloader of test file

--- cifar10_tutorial.py
+++ cifar10_tutorial.py
@@ -194,7 +194,7 @@ print('Finished Training')
 # Okay, first step. Let us display an image from the test set to get familiar.
 
 dataiter = iter(testloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 
 # print images
 # imshow(torchvision.utils.make_grid(images))
